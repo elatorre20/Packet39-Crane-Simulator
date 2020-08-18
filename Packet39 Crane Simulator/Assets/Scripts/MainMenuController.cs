7d6e9da0a765f25eed1a3ac6780abc3341369d28
Emilio Latorre <64216678+elatorre20@users.noreply.github.com>
2020-08-18 00:26:29 -0400

    first functional draft of main menu
    have to add settings menu, highscore system, and fix board model. Also have to add textures for the board and crane models

---
+++ Packet39 Crane Simulator/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    public void showHighScores()
+    {
+
+    }
+}
